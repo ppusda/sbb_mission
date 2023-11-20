258f722a7e5ccbe90b2099ed339963419c9eb355
ppusda <ppusda1234@gmail.com>
2023-11-20 11:14:37 +0900

    feat: init Project

---
+++ README.md
@@ -0,0 +1,3 @@
+### SBB_MISSION PROJECT
+
+점프 투 스프링 부트를 기반으로 구현하는 미션 프로젝트 (https://wikidocs.net/book/7601)
---
+++ src/test/java/com/sbb/sbb_mission/SbbMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbb.sbb_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
