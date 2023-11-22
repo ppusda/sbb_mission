d4ccbba0e7ebff7c8ee5eb6fd61e8facda7b60b3
ppusda <ppusda1234@gmail.com>
2023-11-22 14:30:39 +0900

    fix: svelte version upgrade (4.0 -> 5.0)

--- front/sbb/src/routes/+page.svelte
+++ front/sbb/src/routes/+page.svelte
@@ -1,17 +1,16 @@
 <script>
-	$effect(() => {
-		window.$('body').append("안녕하세요.")
-		window.$('body').click(() => {
-			window.$('body').append("클릭")
-		});
-	});
+
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="홈 화면"/>
+	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section class="text-red-500">
 	<i class="fa-solid fa-user"></i> 메인입니다.
 </section>
+
+<style>
+
+</style>
